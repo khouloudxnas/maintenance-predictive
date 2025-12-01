5347fafe9edf726f90a06079b160a5158dd4fa40
khouloudxnas <khouloudn380@gmail.com>
2025-12-01 03:23:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -653,10 +653,10 @@ elif section == "📖 Ressources":
         """)
     
     with tab3:
-    st.markdown("[Ressources PDF →](https://drive.google.com/drive/folders/1VAjV-7u9YLm3CSw-CJ9mVapuozQis6hP?usp=sharing)")
-    with tab4:
-    st.markdown("- [Kaggle Dataset : Predictive Maintenance](https://www.kaggle.com/datasets)")
-    st.markdown("- [UCI Machine Learning Repository](https://archive.ics.uci.edu/ml/index.php)")
+        st.markdown("[Ressources PDF →](https://drive.google.com/drive/folders/1VAjV-7u9YLm3CSw-CJ9mVapuozQis6hP?usp=sharing)")
+        with tab4:
+        st.markdown("- [Kaggle Dataset : Predictive Maintenance](https://www.kaggle.com/datasets)")
+        st.markdown("- [UCI Machine Learning Repository](https://archive.ics.uci.edu/ml/index.php)")
 
 
 # EXERCICES
@@ -977,6 +977,7 @@ elif section == "ℹ️ À propos":
         et accessible.
 
         """)
+
 
 
 
